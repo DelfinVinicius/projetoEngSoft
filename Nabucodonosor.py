9ec0fe33e25aa862c51127f923948547cadeab05
Vinicius Delfin <62725875+DelfinVinicius@users.noreply.github.com>
2020-10-20 17:58:51 -0300

    Add files via upload

--- Nabucodonosor.py
+++ Nabucodonosor.py
@@ -25,12 +25,17 @@ while True:
         print('-=' * 9, 'CADASTRO', '=-' * 9)
         print('Preencha os campos a seguir com as informações requisitadas!')
         login_cadastro = str(input('Informe o seu email: '))
-        senha_cadastro = int(input('Crie uma senha com oito ou mais números: '))
+        senha_cadastro = int(input('Crie uma senha com oito dígitos: '))
         senha_cadastro_confirmacao = int(input('Digite novamente a senha criada a pouco: '))
+        # Essa parte serve para caso o usuário digite senhas diferentes nos campos
+        while senha_cadastro != senha_cadastro_confirmacao:
+            print('As senhas não coincidem! Tente digite-las novamente.')
+            senha_cadastro = int(input('Senha: '))
+            senha_cadastro_confirmacao = int(input('Digite novamente a senha: '))
         celular_cadastro = str(input('Digite o número do seu celuar: ')).replace(" ", "")
         # Caso o usuário digite um número de celular fora dos padrões esperados
-        while len(celular_cadastro) < 11 or len(celular_cadastro) > 11:
-            print('Número inválido! Tente inserir o DDD ou o "9" logo após o mesmo.')
+        while len(celular_cadastro) < 11 or len(celular_cadastro) > 11 or celular_cadastro not in "1234567890":
+            print('Número inválido! Digite o número novamente.')
             celular_cadastro = str(input('Digite o número do seu celuar: ')).replace(" ", "")
         # Descobrindo a região que o usuário provavelmente mora
         DDD = celular_cadastro[0] + celular_cadastro[1]
@@ -40,12 +45,6 @@ while True:
                 if i == DDD:
                     regiao = lista_cidades_DDDs[c]
                 c += 1
-
-        # Essa parte serve para caso o usuário digite senhas diferentes nos campos
-        while senha_cadastro != senha_cadastro_confirmacao:
-            print('As senhas não coincidem! Tente digite-las novamente.')
-            senha_cadastro = int(input('Senha: '))
-            senha_cadastro_confirmacao = int(input('Digite novamente a senha: '))
         break
 
     # Se o usuário na hora de escolher se logar ou se cadastrar digitar uma opção inválida, essa mensagem aqui aparece
